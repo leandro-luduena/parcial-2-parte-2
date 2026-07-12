cb0f4de30729f3fa3d37ca05bcf16a53a7f6466a
leandro-luduena <luduenaleandro0@gmail.com>
2026-07-12 16:29:09 -0300

    Segundo commit

--- galeria.js
+++ galeria.js
@@ -1,27 +1,90 @@
+// Array con las obras del artista
 const obras = [
 
 {
 nombre:"Pulse Room",
 año:2006,
-imagen:"pulse-room.jpg"
+imagen:"lozano-hemmer-3.jpg"
 },
 
 {
-nombre:"Body Movies",
+nombre:"Sphere Packing: Bach",
 año:2001,
-imagen:"body-movies.jpg"
+imagen:"lozano-hemmer-2.jpg"
 },
 
 {
-nombre:"Vectorial Elevation",
+nombre:"Vicious Circular Breathing",
 año:1999,
-imagen:"vectorial-elevation.jpg"
+imagen:"lozano-hemmer-1.jpg"
 },
 
 {
-nombre:"33 Questions per Minute",
+nombre:"Encode/Decode",
 año:2000,
-imagen:"33questions.jpg"
+imagen:"lozano-hemmer.jpg"
 }
 
 ]; 
+
+const galeria = document.getElementById("galeria");
+const boton = document.getElementById("cambiarDiseno");
+
+boton.addEventListener("click", mostrarGaleria);
+
+// Muestra la galería
+function mostrarGaleria() {
+
+    galeria.innerHTML = "";
+
+    for (let i = 0; i < obras.length; i++) {
+
+        galeria.innerHTML += `
+            <div>
+                <h3>${obras[i].nombre}</h3>
+                <p>${obras[i].año}</p>
+                <img src="${obras[i].imagen}" alt="${obras[i].nombre}">
+            </div>
+        `;
+
+    }
+
+}
+
+let grande = false;
+
+boton.addEventListener("click", cambiarDiseno);
+
+function cambiarDiseno(){
+
+    grande = !grande;
+
+    if(grande){
+
+        galeria.style.backgroundColor = "black";
+        galeria.style.color = "white";
+
+    }else{
+
+        galeria.style.backgroundColor = "white";
+        galeria.style.color = "black";
+
+    }
+
+}
+
+const imagenes = document.querySelectorAll("#galeria img");
+
+for(let i = 0; i < imagenes.length; i++){
+
+    if(grande){
+
+        imagenes[i].style.maxWidth = "300px";
+
+    }else{
+
+        imagenes[i].style.maxWidth = "600px";
+
+    }
+
+}
